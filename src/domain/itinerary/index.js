ae426947539a68dad16002f7d0b2e0294de2b423
Samuel Kitazume <samuel.kitazume@gmail.com>
2019-05-20 23:08:52 -0300

    adding station getting methods to the class.

--- src/domain/itinerary/index.js
+++ src/domain/itinerary/index.js
@@ -1,8 +1,25 @@
 class Itinerary {
-  construct({ name, description='', stations, railways }) {
+  constructor ({ name, description='', stations }) {
     this.name = name
     this.description = description
     this.stations = stations
-    this.railways = railways
+    
+    if (typeof name === 'undefined' || name === null) {
+      const initial = this.getInitial()
+      const itinerary_data = initial.itinerary_data
+      this.name = itinerary_data.name
+      this.description = itinerary_data.description
+    }
+  }
+  getInitial() {
+    return this.stations.find(s=>s.initial===true)
+  }
+  getStationByName(name) {
+    return this.stations.find(s=>s.name===name)
+  }
+  getStationByRid(rid) {
+    return this.stations.find(s=>s['@rid'].toString() == rid)
   }
 }
+
+module.exports = Itinerary
