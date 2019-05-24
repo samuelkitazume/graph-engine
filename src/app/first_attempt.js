e26c8a74d711e6350df5afdf64edd98045d48336
Samuel Kitazume <samuel.kitazume@gmail.com>
2019-05-24 17:08:11 -0300

    Testing new methods

--- src/app/first_attempt.js
+++ src/app/first_attempt.js
@@ -9,16 +9,16 @@ async function test() {
   // await manager.createEnvironment()
 
   let newItinerary = await manager.addItinerary(itineraryExample)
-
+  
   let stations = await Promise.all(itineraryExample.stations.map(async (s) => {
     return manager.createStation(Object.assign({}, s, { itinerary: newItinerary.hash }))
   }))
-
+  
   let stationsObject = stations.reduce((o, s) => {
     o[s.name] = s
     return o
   }, {})
-
+  
   let railwaysObject = itineraryExample.stations.reduce((o, s) => {
     if (s.railways && s.railways.length > 0) {
       for (let railway = s.railways.length - 1; railway > -1; railway--) {
@@ -27,7 +27,7 @@ async function test() {
     }
     return o
   }, {})
-
+  
   let railways = await Promise.all(itineraryExample.railways.map((r) => {
     return manager.linkStations({
       from: stationsObject[railwaysObject[r.name]]['@rid'].toString(),
@@ -36,15 +36,37 @@ async function test() {
       receipt: r.bullet
     })
   }))
-
-  console.log(newItinerary, stations, railways)
-
+  
+  
+  const itineraries = await manager.getItineraries()
   let passenger = buildPassenger(newItinerary.hash)
-
   let newPassenger = await manager.createPassenger(passenger)
-
+  
+  console.log('new itinerary', newItinerary)
+  console.log('itineraries', itineraries)
   console.log('passenger', newPassenger)
 
 }
 
-test()
+// test()
+
+async function movePass({ hash, ticket }) {
+  let newStation = await manager.movePassenger({ hash, ticket })
+  console.log(newStation)
+}
+
+movePass({ hash: 'b294fb58', ticket: 'trigger9' })
+
+async function checkTicket({ hash, ticket }) {
+  let newStation = await manager.checkTicket({ hash, ticket })
+  console.log(newStation)
+}
+
+// checkTicket({ hash: '65889510', ticket: 'trigger9' })
+
+async function getTickets({ hash }) {
+  let newStation = await manager.getTickets({ hash })
+  console.log('tickets',newStation)
+}
+
+// getTickets({ hash: '65889510' })
