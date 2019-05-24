742b3ceb8d768a1823d948b1f217eaef5ddcd845
Samuel Kitazume <samuel.kitazume@gmail.com>
2019-05-24 18:06:56 -0300

    creating Itinerary Controller

---
+++ src/app/itinerary/ItineraryController.js
@@ -0,0 +1,47 @@
+class ItineraryController {
+  
+  constructor({ manager }) {
+    this.manager = manager
+  }
+  
+  async createItinerary(o) {
+  
+    let newItinerary = await manager.addItinerary(o)
+    
+    let stations = await Promise.all(o.stations.map(async (s) => {
+      return manager.createStation(Object.assign({}, s, { itinerary: newItinerary.hash }))
+    }))
+    
+    let stationsObject = stations.reduce((o, s) => {
+      o[s.name] = s
+      return o
+    }, {})
+    
+    let railwaysObject = o.stations.reduce((o, s) => {
+      if (s.railways && s.railways.length > 0) {
+        for (let railway = s.railways.length - 1; railway > -1; railway--) {
+          o[s.railways[railway]] = s.name
+        }
+      }
+      return o
+    }, {})
+    
+    await Promise.all(o.railways.map((r) => {
+      return manager.linkStations({
+        from: stationsObject[railwaysObject[r.name]]['@rid'].toString(),
+        to: stationsObject[r.destination]['@rid'].toString(),
+        ticket: r.trigger,
+        receipt: r.bullet
+      })
+    }))
+    
+    return newItinerary
+  }
+
+  async getItineraries() {
+    return await this.manager.getItineraries()
+  }
+  
+}
+
+module.exports = ItineraryController
